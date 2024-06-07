85387860cbcff2b97435f0c33e43451ab050af34
Ardi Sasongko <ardisasongko002@gmail.com>
2024-06-07 19:50:15 +0700

    fix event

--- repository/event/event_repository.go
+++ repository/event/event_repository.go
@@ -3,6 +3,6 @@ package event_repository
 import "github.com/ArdiSasongko/ticketing_app/model/domain"
 
 type EventRepository interface {
-	GetEvent(Id int) (domain.Events, error)
-	GetEvents() ([]domain.Events, error)
+	GetEvent(Id int) (domain.Event, error)
+	GetEvents() ([]domain.Event, error)
 }
