93663054f58a339430dd2a3e6fba842091178644
William Antoline <65105895+williamantoline@users.noreply.github.com>
2024-06-09 22:07:09 +0700

    use env

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ArdiSasongko/ticketing_app/route"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"log"
+	"os"
 )
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("error loading .env file!")
+	}
+
 	r := echo.New()
+
 	route.RegisterBuyerRoutes("/buyer", r)
 	route.RegisterSellerRoutes("/seller", r)
 
-	r.Logger.Fatal(r.Start(":8001"))
+	r.Debug = true
+	r.Logger.Fatal(r.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
