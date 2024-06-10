634e68d57abd8e59d8ea171c047041ea35a90da5
Ardi Sasongko <ardisasongko002@gmail.com>
2024-06-10 18:51:45 +0700

    fix

--- app/database.go
+++ app/database.go
@@ -1,14 +1,15 @@
 package app
 
 import (
+	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
 )
 
 func DBConnection() *gorm.DB {
 
-	dsn := "user=postgres password=123 dbname=test port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := "user=postgres password=mypassword dbname=test port=5432 sslmode=disable TimeZone=Asia/Jakarta"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
