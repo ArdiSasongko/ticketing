9ffb7358868f4ca98a60c8bb9bf033667360082c
William Antoline <65105895+williamantoline@users.noreply.github.com>
2024-06-10 19:24:58 +0700

    merge origin/dev-fitur-jwt

--- route/buyer_route.go
+++ route/buyer_route.go
@@ -2,12 +2,12 @@ package route
 
 import (
 	"github.com/ArdiSasongko/ticketing_app/app"
-	buyer_controller "github.com/ArdiSasongko/ticketing_app/controller/buyer"
-	helper "github.com/ArdiSasongko/ticketing_app/helper"
+	"github.com/ArdiSasongko/ticketing_app/controller/buyer"
+	"github.com/ArdiSasongko/ticketing_app/helper"
 	"github.com/ArdiSasongko/ticketing_app/middleware"
-	buyer_query_builder "github.com/ArdiSasongko/ticketing_app/query_builder/buyer"
-	buyer_repository "github.com/ArdiSasongko/ticketing_app/repository/buyer"
-	buyer_service "github.com/ArdiSasongko/ticketing_app/service/buyer"
+	"github.com/ArdiSasongko/ticketing_app/query_builder/buyer"
+	"github.com/ArdiSasongko/ticketing_app/repository/buyer"
+	"github.com/ArdiSasongko/ticketing_app/service/buyer"
 	"github.com/labstack/echo/v4"
 )
 
@@ -31,6 +31,6 @@ func RegisterBuyerRoutes(prefix string, e *echo.Echo) {
 	meRoute := g.Group("/me", middleware.JWTProtection())
 	meRoute.POST("/update", buyerAuthController.Update)
 
-	eventRoute := g.Group("/events")
+	eventRoute := g.Group("/events", middleware.JWTProtection())
 	eventRoute.GET("", buyerEventController.GetEventList)
 }
