f250a145b2e7178eae8807e6c3c15d3897840f43
NaufalF22 <63091744+NaufalF22@users.noreply.github.com>
2024-06-14 18:58:23 +0700

    Masih belum fix bagian order

--- route/buyer_route.go
+++ route/buyer_route.go
@@ -9,6 +9,7 @@ import (
 	buyer_query_builder "github.com/ArdiSasongko/ticketing_app/query_builder/buyer"
 	buyer_repository "github.com/ArdiSasongko/ticketing_app/repository/buyer"
 	history_repository "github.com/ArdiSasongko/ticketing_app/repository/history"
+	order_repository "github.com/ArdiSasongko/ticketing_app/repository/order"
 	buyer_service "github.com/ArdiSasongko/ticketing_app/service/buyer"
 	order_service "github.com/ArdiSasongko/ticketing_app/service/order"
 	"github.com/labstack/echo/v4"
@@ -25,7 +26,7 @@ func RegisterBuyerRoutes(prefix string, e *echo.Echo) {
 	buyerEventRepo := buyer_repository.NewEventRepository(buyerEventQB)
 	buyerEventService := buyer_service.NewEventService(buyerEventRepo)
 	buyerEventController := buyer_controller.NewEventController(buyerEventService)
-	orderRepository := buyer_repository.NewEventRepository(db)
+	orderRepository := order_repository.NewOrderRepository(db)
 	orderService := order_service.NewOrderService(orderRepository, token)
 	orderController := order_controller.NewOrderController(orderService)
 	g := e.Group(prefix)
