8efb581d5d3aa311342c6138a3f3e4a7ad0189b9
William Antoline <65105895+williamantoline@users.noreply.github.com>
2024-06-23 15:35:07 +0700

    add auth + role middleware

--- middleware/jwt_protection.go
+++ middleware/jwt_protection.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ArdiSasongko/ticketing_app/helper"
 	"github.com/ArdiSasongko/ticketing_app/model"
+	"github.com/ArdiSasongko/ticketing_app/model/enum"
 	"github.com/golang-jwt/jwt/v5"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
@@ -18,7 +19,29 @@ func JWTProtection() echo.MiddlewareFunc {
 		},
 		SigningKey: []byte(os.Getenv("SECRET_KEY")),
 		ErrorHandler: func(c echo.Context, err error) error {
-			return c.JSON(http.StatusUnauthorized, model.ResponseToClient(http.StatusUnauthorized, "anda harus login untuk mengakses resource ini", nil))
+			return c.JSON(http.StatusUnauthorized, model.ResponseToClient(http.StatusUnauthorized, "unauthorized", nil))
 		},
 	})
+}
+
+func auth(user enum.UserType, next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		claims := c.Get("user").(*jwt.Token).Claims.(*helper.JwtCustomClaims)
+		if claims.Role != string(user) {
+			return c.JSON(http.StatusForbidden, map[string]string{"message": "Access forbidden"})
+		}
+		return next(c)
+	}
+}
+
+func IsBuyer(next echo.HandlerFunc) echo.HandlerFunc {
+	return auth(enum.UserTypeBuyer, next)
+}
+
+func IsSeller(next echo.HandlerFunc) echo.HandlerFunc {
+	return auth(enum.UserTypeSeller, next)
+}
+
+func IsAdmin(next echo.HandlerFunc) echo.HandlerFunc {
+	return auth(enum.UserTypeAdmin, next)
 }
